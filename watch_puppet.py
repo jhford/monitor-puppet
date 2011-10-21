da7a0ae4c6b5c23e9cab4b9ec43b0e5b561b6059
John Ford <john@johnford.info>
2011-10-21 13:49:49 -0700

    Only figure out hostname at beginning of program

--- watch_puppet.py
+++ watch_puppet.py
@@ -18,9 +18,7 @@ invalid_cert = re.compile('(?P<datetime>\w*\W*\d* \d*:\d*:\d*) (?P<p_master>[\w.
 waiting_cert = re.compile('(?P<datetime>\w*\W*\d* \d*:\d*:\d*) (?P<p_master>[\w.\-_]*) puppetmasterd\[\d*\]: Host (?P<hostname>[\w\d\-_.]*) has a waiting certificate request')
 suggestion_parse = re.compile('puppetca --clean (?P<hostname>[\w\d\-._]*)')
 
-def email(subject,body,to='jhford@mozilla.com', sender=None):
-    if sender is None:
-        sender = '%s@%s' % (os.getlogin(), socket.gethostname())
+def email(subject,body,to,sender):
     msg = MIMEText(body)
     msg['Subject'] = subject
     msg['To'] = to
@@ -29,7 +27,7 @@ def email(subject,body,to='jhford@mozilla.com', sender=None):
     s = smtplib.SMTP('localhost')
     s.sendmail(sender, to, msg.as_string())
 
-def handle_invalid_cert(p_master, datetime, suggestion):
+def handle_invalid_cert(p_master, datetime, suggestion, to, sender):
     hostname = suggestion_parse.match(suggestion).group("hostname")
     subprocess.call(['puppetca', '--clean', hostname])
     subject = "[%s] %s has invalid cert" % (p_master, hostname)
@@ -37,24 +35,25 @@ def handle_invalid_cert(p_master, datetime, suggestion):
 I tried cleaning it up manually, but I didn't check return codes.
 Please make sure that this was done properly.  I won't automatically
 try to sign a slave for security, so please sign it yourself!""" %hostname
-    email(subject,body)
+    email(subject,body,to,sender)
 
 
-def handle_waiting_cert(p_master, datetime, hostname):
+def handle_waiting_cert(p_master, datetime, hostname, to, sender):
     subject = "[%s] %s is waiting to be signed" % (p_master, hostname)
     body = """The puppet slave '%s' has a waiting puppet signing request
 I won't automatically try to sign a slave for security, so please
 sign it yourself!""" %hostname
-    email(subject,body)
+    email(subject,body,to,sender)
 
 
-def process_line(line):
+def process_line(line, to, sender):
     m = invalid_cert.match(line)
     if m:
         handle_invalid_cert(
             m.group("p_master"),
             m.group("datetime"),
-            m.group("suggestion")
+            m.group("suggestion"),
+            to, sender
         )
         return True
     m = waiting_cert.match(line)
@@ -62,12 +61,13 @@ def process_line(line):
         handle_waiting_cert(
             m.group("p_master"),
             m.group("datetime"),
-            m.group("hostname")
+            m.group("hostname"),
+            to, sender
         )
         return True
 
 
-def watch(filename):
+def watch(filename, to, sender):
     log = open(filename)
     log.seek(0,2) # Only want to look at *new* things
     while True:
@@ -75,19 +75,20 @@ def watch(filename):
             data = log.readline().strip()
             if data == '':
                 time.sleep(0.5)
-            process_line(data)
+            process_line(data, to, sender)
         except IOError:
             log = open(filename)
             log.seek(0,2) # Only want to look at *new* things
     log.close()
 
 def main():
+    sender = '%s@%s' % (os.getlogin(), socket.gethostname())
     if len(sys.argv) != 2:
         print "usage: %s /var/log/messages" % sys.argv[0]
         exit(1)
     else:
         print "Watching %s" % sys.argv[1]
-        watch(sys.argv[1])
+        watch(sys.argv[1], sys.argv[1], sender)
 
 
 if __name__ == "__main__":
