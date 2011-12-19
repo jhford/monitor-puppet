5b38be908b4f885128d470388eb2913b0657d4d2
John Ford <john@johnford.info>
2011-12-19 13:22:58 -0500

    organize code in a more extenable way by throwing, fix bug in exception handler

--- watch_puppet.py
+++ watch_puppet.py
@@ -13,12 +13,67 @@ except ImportError:
     # Old versions of python don't use the new module names
     from email.MIMEText import MIMEText
 
-# These regexes and functions should be abstracted.  Maybe a
-# class with Handler.check(line) and Handler.handle(line)
 
-invalid_cert = re.compile('(?P<datetime>\w*\W*\d* \d*:\d*:\d*) (?P<p_master>[\w.\-_]*) puppetmasterd\[\d*\]: Certificate request does not match existing certificate; run \'(?P<suggestion>.*)\'.')
-waiting_cert = re.compile('(?P<datetime>\w*\W*\d* \d*:\d*:\d*) (?P<p_master>[\w.\-_]*) puppetmasterd\[\d*\]: Host (?P<hostname>[\w\d\-_.]*) has a waiting certificate request')
-suggestion_parse = re.compile('puppetca --clean (?P<hostname>[\w\d\-._]*)')
+class Handler(object):
+    """check for and handle an issue.  The default handler expects
+    a regex pattern with at least the following groups:
+        'hostname': hostname of the puppet slave
+        'p_master': puppet master name of the puppet master"""
+
+    subject = "unknown error on %(hostname)s"
+    body = "There was an error on %(hostname)s"
+    pattern = None
+
+    def __init__(self, to, sender):
+        object.__init__(self)
+        self.to = to
+        self.sender = sender
+
+    def check(self, line):
+        m = self.pattern.match(line)
+        if m:
+            self.handle(line, m.groupdict())
+
+    def handle(self, line, match_dict):
+        email("[puppet-monitoring][%(p_master)s] " % match_dict + self.subject % match_dict,
+              self.body % match_dict + "\nRAW LOG:\n%s" % line, self.to, self.sender)
+
+class InvalidCertHandler(Handler):
+    subject = "%(hostname)s has an invalid certificate"
+    body  = "The puppet slave %(hostname)s has an invalid puppet master certificate." + \
+            "An attempt to clean it has been made, but no follow up has been done"
+    pattern = re.compile('(?P<datetime>\w*\W*\d* \d*:\d*:\d*) (?P<p_master>[\w.\-_]*) puppetmasterd\[\d*\]: Certificate request does not match existing certificate; run \'(?P<suggestion>puppetca --clean (?P<hostname>[\w\d\-._]*))\'.')
+    suggestion_parse = re.compile('puppetca --clean (?P<hostname>[\w\d\-._]*)')
+
+    def __init__(self, to, sender):
+        Handler.__init__(self, to, sender)
+
+    def handle(self, line, match_dict):
+        nodename = self.suggestion_parse.match(match_dict['suggestion']).group("hostname")
+        rc = subprocess.call(['puppetca', '--clean', nodename])
+        print "Cleaning puppetca on %s had rc of %d" % (nodename, rc)
+        # TODO figure out a way to send rc
+        Handler.handle(self, line, match_dict)
+
+class WaitingCertHandler(Handler):
+    subject = "%(hostname)s is waiting to be signed"
+    body = "%(hostname)s is waiting to be signed.  It will probably be signed by " + \
+           "accept-hostname-keys.sh"
+    pattern = re.compile('(?P<datetime>\w*\W*\d* \d*:\d*:\d*) (?P<p_master>[\w.\-_]*) puppetmasterd\[\d*\]: Host (?P<hostname>[\w\d\-_.]*) has a waiting certificate request')
+
+    def __init__(self, to, sender):
+        Handler.__init__(self, to, sender)
+
+
+class UnknownSlaveHandler(Handler):
+    subject = "%(hostname)s is unknown to %(p_master)s"
+    body = "%(hostname)s is not known to %(p_master)s but is trying to sync with it."
+    pattern = re.compile("(?P<datetime>\w*\W*\d* \d*:\d*:\d*) (?P<p_master>[\w.\-_]*) puppetmasterd\[\d*\]: Could not find default node or by name with '(?P<candidates>.*)' on node (?P<hostname>.*)")
+
+    def __init__(self, to, sender):
+        Handler.__init__(self, to, sender)
+
+
 
 def email(subject,body,to,sender):
     msg = MIMEText(body)
@@ -29,59 +84,30 @@ def email(subject,body,to,sender):
     s = smtplib.SMTP('localhost')
     s.sendmail(sender, to, msg.as_string())
 
-def handle_invalid_cert(p_master, datetime, suggestion, to, sender):
-    hostname = suggestion_parse.match(suggestion).group("hostname")
-    subprocess.call(['puppetca', '--clean', hostname])
-    subject = "[%s] %s has invalid cert" % (p_master, hostname)
-    body = """The puppet slave '%s' has an invalid puppet cert.
-I tried cleaning it up manually, but I didn't check return codes.
-Please make sure that this was done properly.""" %hostname
-    email(subject,body,to,sender)
-
-
-def handle_waiting_cert(p_master, datetime, hostname, to, sender):
-    subject = "[%s] %s is waiting to be signed" % (p_master, hostname)
-    body = """The puppet slave '%s' has a waiting puppet signing request.
-It will probably be signed automatically by accept-hostname-keys.sh""" %hostname
-    email(subject,body,to,sender)
-
-
-def process_line(line, to, sender):
-    m = invalid_cert.match(line)
-    if m:
-        handle_invalid_cert(
-            m.group("p_master"),
-            m.group("datetime"),
-            m.group("suggestion"),
-            to, sender
-        )
-        return True
-    m = waiting_cert.match(line)
-    if m:
-        handle_waiting_cert(
-            m.group("p_master"),
-            m.group("datetime"),
-            m.group("hostname"),
-            to, sender
-        )
-        return True
-
 
 def watch(filename, to, sender):
     log = open(filename)
     log.seek(0,2) # Only want to look at *new* things
+    handlers = [InvalidCertHandler(to, sender),
+                WaitingCertHandler(to, sender),
+                UnknownSlaveHandler(to, sender)]
     while True:
         try:
-            data = log.readline().strip()
-            if data == '':
-                time.sleep(0.5)
-            process_line(data, to, sender)
-        except IOError:
-            # There is a bug here because failing to open the file
-            # the second time isn't caught and is fatal
             log = open(filename)
             log.seek(0,2) # Only want to look at *new* things
-    log.close()
+            while True:
+                try:
+                    data = log.readline().strip()
+                    if data == '':
+                        time.sleep(0.5)
+                    for i in handlers:
+                        i.check(data)
+                except IOError:
+                    if not f.closed():
+                        f.close()
+        except IOError:
+            time.sleep(0.5)
+            pass
 
 def main():
     sender = '%s@%s' % (os.getlogin(), socket.gethostname())
